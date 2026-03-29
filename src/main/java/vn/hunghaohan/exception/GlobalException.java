a8a595d3c60cc025fa7f0f86c797b9db31cbd301
chishiya2309 <lequanghung.work@gmail.com>
2026-03-29 22:31:11 +0700

    Catch exception

---
+++ src/main/java/vn/hunghaohan/exception/GlobalException.java
@@ -0,0 +1,100 @@
+package vn.hunghaohan.exception;
+
+    import io.swagger.v3.oas.annotations.media.Content;
+    import io.swagger.v3.oas.annotations.media.ExampleObject;
+    import io.swagger.v3.oas.annotations.responses.ApiResponse;
+    import io.swagger.v3.oas.annotations.responses.ApiResponses;
+    import jakarta.validation.ConstraintViolationException;
+    import org.springframework.web.bind.MethodArgumentNotValidException;
+    import org.springframework.web.bind.MissingServletRequestParameterException;
+    import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+    import static org.springframework.http.HttpStatus.BAD_REQUEST;
+    import static org.springframework.http.HttpStatus.NOT_FOUND;
+    import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestControllerAdvice
+public class GlobalException {
+
+    @ExceptionHandler({ConstraintViolationException.class,
+        MissingServletRequestParameterException.class, MethodArgumentNotValidException.class})
+    @ResponseStatus(BAD_REQUEST)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "400", description = "Bad Request",
+                content = {@Content(mediaType = APPLICATION_JSON_VALUE,
+                    examples = @ExampleObject(
+                            name = "Handle exception when the data invalid. (@RequestBody, @RequestParam)",
+                            summary = "Handle Bad Request",
+                            value = """
+                                    {
+                                        "timestamp": "2023-10-19T06:07:35.321+00:00",
+                                        "status": 400,
+                                        "path": "/api/v1/...",
+                                        "error": "Invalid payload",
+                                        "message": "{data} must not be blank"
+                                    }
+                                    """
+                    ))})
+    })
+    public ErrorResponse handleValidationException(Exception e, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setTimestamp(new Date());
+        errorResponse.setStatus(BAD_REQUEST.value());
+        errorResponse.setPath(request.getDescription(false).replace("uri=", ""));
+
+        String message = e.getMessage();
+        if (e instanceof MethodArgumentNotValidException) {
+            int start = message.lastIndexOf("[") + 1;
+            int end = message.lastIndexOf("]") - 1;
+            message = message.substring(start, end);
+            errorResponse.setError("Invalid Payload");
+            errorResponse.setMessage(message);
+        }else if(e instanceof MissingServletRequestParameterException) {
+            errorResponse.setError("Invalid Parameter");
+            errorResponse.setMessage(message);
+        }else if(e instanceof ConstraintViolationException) {
+            errorResponse.setError("Invalid Parameter");
+            errorResponse.setMessage(message.substring(message.indexOf(" ") + 1));
+        }else {
+            errorResponse.setError("Invalid Data");
+            errorResponse.setMessage(message);
+        }
+
+        return errorResponse;
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(NOT_FOUND)
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404", description = "Bad Request",
+                content = {@Content(mediaType = APPLICATION_JSON_VALUE,
+                    examples = @ExampleObject(
+                            name = "404 Response",
+                            summary = "Handle exception when resouce not found",
+                            value = """
+                                    {
+                                        "timestamp": "2023-10-19T06:07:35.321+00:00",
+                                        "status": 404,
+                                        "path": "/api/v1/...",
+                                        "error": "Not Found",
+                                        "message": "{data} not found"
+                                    }
+                                    """
+                    ))})
+    })
+    public ErrorResponse handleResourceNotFoundException(ResourceNotFoundException e, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setTimestamp(new Date());
+        errorResponse.setPath(request.getDescription(false).replace("uri=", ""));
+        errorResponse.setStatus(NOT_FOUND.value());
+        errorResponse.setError(NOT_FOUND.getReasonPhrase());
+        errorResponse.setMessage(e.getMessage());
+
+        return errorResponse;
+    }
+}
